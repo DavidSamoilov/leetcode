14098b338beb31d20eafc698a9a5a116203be797
DavidSamoilov <66166375+DavidSamoilov@users.noreply.github.com>
2022-12-07 20:55:14 +0200

    Time: 98 ms (63.42%), Space: 44.8 MB (83.82%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    
+   
+   let left = 0,  right = nums.length -1;
+    
+    while(left <= right){
+        let middle =  Math.floor((right + left)/2)
+        
+        if(nums[middle] === target){
+            return middle
+        }else if(nums[middle] < target){
+            left = middle + 1
+        }else if(nums[middle] > target){
+            right = middle - 1
+        }
+    }
+    return -1
+};
